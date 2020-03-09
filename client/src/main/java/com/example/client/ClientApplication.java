5e801a8c4b7dde3296a0ae07d3d8c9bc644c7c02
Vovamzur <vovamazur3@gmail.com>
2020-03-09 15:51:29 +0200

    implement right api-getaway

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -3,13 +3,11 @@ package com.example.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@RestController
 public class ClientApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ClientApplication.class, args);
+	    SpringApplication.run(ClientApplication.class, args);
 	}
 }
--- getaway/src/main/java/com/example/getaway/GetawayApplication.java
+++ getaway/src/main/java/com/example/getaway/GetawayApplication.java
@@ -3,42 +3,25 @@ package com.example.getaway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-//@EnableEurekaClient
 public class GetawayApplication {
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p
+//                        .path("/countries/**")
+//                        .filters(f -> f.stripPrefix(1))
+//                        .uri("lb://countries-service/api/countries")
+//                        .id("countries-service")
+//                )
+//                .build();
+//    }
 
-//	@Bean
-//	@LoadBalanced
-//	public RestTemplate restTemplate () {
-//		return new RestTemplate();
-//	}
-//
-//	@Bean
-//	public WebClient.Builder getWebClientBuilder () {
-//		return WebClient.builder();
-//	}
 
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/get")
-						.uri("lb://countries-service/api/countries")
-				)
-				.build();
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(GetawayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GetawayApplication.class, args);
+    }
 
 }
