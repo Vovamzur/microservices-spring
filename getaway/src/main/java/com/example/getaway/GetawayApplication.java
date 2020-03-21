6bc2dc2ae579b488c7ac0b268653e99e08986ffb
Vovamzur <vovamazur3@gmail.com>
2020-03-21 12:20:44 +0200

    attempt to change remote url

--- getaway/src/main/java/com/example/getaway/GetawayApplication.java
+++ getaway/src/main/java/com/example/getaway/GetawayApplication.java
@@ -7,21 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GetawayApplication {
-//    @Bean
-//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(p -> p
-//                        .path("/countries/**")
-//                        .filters(f -> f.stripPrefix(1))
-//                        .uri("lb://countries-service/api/countries")
-//                        .id("countries-service")
-//                )
-//                .build();
-//    }
-
-
     public static void main(String[] args) {
         SpringApplication.run(GetawayApplication.class, args);
     }
-
 }
